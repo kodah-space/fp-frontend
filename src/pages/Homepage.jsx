4b0ea5d8abc0b5987d72b981a3df175164d5dda8
Mira Witte <mira.witte@gmx.de>
2024-05-27 18:19:14 +0200

    styling hexagon

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -6,28 +6,36 @@ const API_URL = "http://localhost:5005";
 
 function Homepage() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">HOMEPAGE</h1>
-      <img className="mask mask-hexagon" src="https://picsum.photos/536/354" />
-      <div className="flex">
-        {" "}
-        <div>
-          {" "}
-          <div className="w-[100px] h-[50px] relative">
-            {" "}
-            <div className="absolute top-[25px] border-l-[50px] border-r-[50px] border-b-[25px]"></div>{" "}
-            <div className="bottom-[25px] border-l-[50px] border-r-[50px] border-b-[25px] border-l-transparent border-r-transparent"></div>{" "}
-          </div>{" "}
-          <div className="w-[100px] rotate-180 h-[50px] relative">
-            {" "}
-            <div className="absolute top-[25px] border-l-[50px] border-r-[50px] border-b-[25px]"></div>{" "}
-            <div className="bottom-[25px] border-l-[50px] border-r-[50px] border-b-[25px] border-l-transparent border-r-transparent"></div>{" "}
-            <img src="https://picsum.photos/536/354" />
-          </div>{" "}
-        </div>{" "}
+    <div className="text-center mt-10">
+      <h1 className="text-3xl font-bold underline mb-4">HOMEPAGE</h1>
+      <div className="flex justify-center">
+        <div className="hexagon-container relative overflow-hidden">
+          <svg
+            className="absolute inset-0 w-full h-full"
+            viewBox="0 0 100 115.47"
+          >
+            <clipPath id="clip">
+              <polygon points="50,10 100,27 100,88 50,105 0,88 0,27"></polygon>
+            </clipPath>
+            <image
+              href="https://picsum.photos/536/354"
+              x="0"
+              y="0"
+              height="58px"
+              width="100"
+              clipPath="url(#clip)"
+            />
+            <g clipPath="url(#clip)">
+              <foreignObject x="0" y="58px" width="100" height="57.74">
+                <div className="bg-white w-full h-full flex items-center justify-center p-4">
+                  <p>Text content goes here</p>
+                </div>
+              </foreignObject>
+            </g>
+          </svg>
+        </div>
       </div>
     </div>
   );
 }
-
 export default Homepage;
