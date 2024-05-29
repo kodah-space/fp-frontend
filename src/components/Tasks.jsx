7622ec314eb0de06cc4b7303acce2784f2a51c4f
Mira Witte <mira.witte@gmx.de>
2024-05-29 18:53:28 +0200

    form styling

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,43 +1,43 @@
-import React, { useState, useEffect } from "react";
-import TaskCard from "./TaskCard";
-import TaskServices from "../context/TaskServices";
+// import React, { useState, useEffect } from "react";
+// import TaskCard from "./TaskCard";
+// import TaskServices from "../context/TaskServices";
 
-export default function Tasks() {
-  const [tasksConnectedToEvent, setTasksConnectedToEvent] = useState(null);
-  const [tasksUserSignedupFor, setTasksUserSignedupFor] = useState(null);
+// export default function Tasks() {
+//   const [tasksConnectedToEvent, setTasksConnectedToEvent] = useState(null);
+//   const [tasksUserSignedupFor, setTasksUserSignedupFor] = useState(null);
 
-  useEffect(() => {
-    // Fetch tasks created for an event
-    TaskServices.getTasksConnectedToEvent()
-      .then((resp) => {
-        setTasksConnectedToEvent(resp.data);
-      })
-      .catch((error) => console.error("Failed to fetch related tasks:", error));
+//   useEffect(() => {
+//     // Fetch tasks created for an event
+//     TaskServices.getTasksConnectedToEvent()
+//       .then((resp) => {
+//         setTasksConnectedToEvent(resp.data);
+//       })
+//       .catch((error) => console.error("Failed to fetch related tasks:", error));
 
-    // Fetch tasks the user signedup for
-    TaskServices.getTasksUserSignedupFor()
-      .then((resp) => {
-        setTasksUserSignedupFor(resp.data);
-      })
-      .catch((error) => console.error("Failed to fetch user tasks:", error));
-  }, []);
+//     // Fetch tasks the user signedup for
+//     TaskServices.getTasksUserSignedupFor()
+//       .then((resp) => {
+//         setTasksUserSignedupFor(resp.data);
+//       })
+//       .catch((error) => console.error("Failed to fetch user tasks:", error));
+//   }, []);
 
-  console.log("Tasks related to event:", tasksConnectedToEvent);
-  console.log("Tasks User signedup for:", tasksUserSignedupFor);
+//   console.log("Tasks related to event:", tasksConnectedToEvent);
+//   console.log("Tasks User signedup for:", tasksUserSignedupFor);
 
-  if (!tasksConnectedToEvent || !tasksUserSignedupFor) return "loading...";
+//   if (!tasksConnectedToEvent || !tasksUserSignedupFor) return "loading...";
 
-  return (
-    <div>
-      <h2>Tasks</h2>
-      <div>
-        {tasksConnectedToEvent.map((task) => (
-          <TaskCard key={task.id} task={task} />
-        ))}
-        {tasksUserSignedupFor.map((task) => (
-          <TaskCard key={task.id} task={task} />
-        ))}
-      </div>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <h2>Tasks</h2>
+//       <div>
+//         {tasksConnectedToEvent.map((task) => (
+//           <TaskCard key={task.id} task={task} />
+//         ))}
+//         {tasksUserSignedupFor.map((task) => (
+//           <TaskCard key={task.id} task={task} />
+//         ))}
+//       </div>
+//     </div>
+//   );
+// }
