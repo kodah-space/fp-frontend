ddc04c23cbbf1dd9e182ee4ec17855dd1f30eabc
Mira Witte <mira.witte@gmx.de>
2024-05-31 07:54:59 +0200

    add project texts

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -44,10 +44,11 @@ function Homepage() {
       )}
 
       <p className="px-4">
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
-        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
-        voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
-        clita kasd gubergren.
+        You have a plans? A festival in the woods? A songwriting workshop with
+        your music fellows? Grandma’s 80s? <br />
+        kandi helps you to build communities of people who share the same
+        interests and organize community events. Find your people and share your
+        skills.
       </p>
       <br />
 
@@ -68,17 +69,21 @@ function Homepage() {
       <div className="border-2 mx-5 mt-5 mb-24 border-[#E2DD68] text-left">
         <h2 className="font-bold text-xl underline p-4">Community Rules</h2>
         <p className="text-sm px-4 pb-4">
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
-          rebum. <br /> <br />
-          Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum
-          dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing
-          elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore
-          magna aliquyam erat, sed diam voluptua. <br /> <br />
-          At vero eos et accusam et justo duo dolores et ea rebum. Stet clita
-          kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
-          amet.
+          kandi is dedicated to providing a harassment-free experience for
+          everyone. We prioritize marginalized people’s safety over privileged
+          people’s comfort. This includes taking seriously reasonable
+          communication of boundaries. <br />
+          <br />
+          Harassment includes: Offensive comments related to gender, gender
+          identity and expression, sexual orientation, disability, mental
+          illness, neuro(a)typicality, physical appearance, age, race, or
+          religion Unwelcome comments regarding a person’s lifestyle choices and
+          practices, including those related to food, health, parenting, drugs,
+          and employment Threats of violence Deliberate intimidation Stalking or
+          following Sustained disruption of discussion Unwelcome sexual
+          attention as well as patterns of inappropriate social contact
+          Publication of non-harassing private communication Deliberate
+          misgendering or use of ‘dead’ or rejected names
         </p>
       </div>
     </div>
